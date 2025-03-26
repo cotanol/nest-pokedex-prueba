9399c9e652430c9ac66e4e0bd03015ca93c68cd1
cotanol <salcedomoralesfranciscojose@gmail.com>
2025-03-25 23:51:19 -0500

    Antes de Lotes

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  excuteSEED() {
-    return 'Execute SEED';
+  //Para hacer saber que tenemos una dependencia de Axios
+  private readonly axios: AxiosInstance = axios;
+
+  async excuteSEED() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segment = url.split('/');
+      const no = +segment[segment.length - 2];
+      console.log(`No: ${no} - Name: ${name}`);
+    });
+
+    return data.results;
   }
 }
